310ce58696cddc7c62424d60a5d122cb08520043
officialharikrishnan <harikrishnanpnr001@gmail.com>
2023-04-22 23:57:15 +0530

    infra

--- order-service/src/frameworks/database/mongoose/connection.ts
+++ order-service/src/frameworks/database/mongoose/connection.ts
@@ -1,7 +1,9 @@
 import mongoose, {model,Schema} from 'mongoose'
+import * as dotenv from 'dotenv'
+dotenv.config()
 const DbName:string='Clean_orders'
 export const DbConnection =async () => {
-    await mongoose.connect(`mongodb://localhost:27017/${DbName}`)
+    await mongoose.connect(`mongodb+srv://Harikrishnan:${process.env.Mongo_Pass}@cluster0.eafyto8.mongodb.net/?retryWrites=true&w=majority`)
     console.log("database connected");
     
 }
