df88606debcd228746691aa1d640d355aca33e93
officialharikrishnan <harikrishnanpnr001@gmail.com>
2023-04-23 00:04:38 +0530

    dotenv testing

--- product-service/src/framework/database/mongoose/connection.ts
+++ product-service/src/framework/database/mongoose/connection.ts
@@ -1,7 +1,11 @@
 import mongoose, { Connection } from "mongoose";
+import * as dotenv from 'dotenv'
+dotenv.config()
 const DbName:string='Clean_Product'
 export const DbConnection =async () => {
-    await mongoose.connect(`mongodb://localhost:27017/${DbName}`) // mongoose connection
+    console.log(process.env.Mongo_Pass);
+    
+    await mongoose.connect(`mongodb+srv://Harikrishnan:${process.env.Mongo_Pass}@cluster0.eafyto8.mongodb.net/?retryWrites=true&w=majority`) // mongoose connection
     console.log("Database Connected");
     
 }
