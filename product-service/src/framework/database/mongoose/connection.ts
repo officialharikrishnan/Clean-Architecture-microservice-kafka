6cc3ceab6ee5151c6f37507f4ead40270229f016
officialharikrishnan <harikrishnanpnr001@gmail.com>
2023-04-23 00:07:25 +0530

    db connected to cloud

--- product-service/src/framework/database/mongoose/connection.ts
+++ product-service/src/framework/database/mongoose/connection.ts
@@ -3,8 +3,6 @@ import * as dotenv from 'dotenv'
 dotenv.config()
 const DbName:string='Clean_Product'
 export const DbConnection =async () => {
-    console.log(process.env.Mongo_Pass);
-    
     await mongoose.connect(`mongodb+srv://Harikrishnan:${process.env.Mongo_Pass}@cluster0.eafyto8.mongodb.net/?retryWrites=true&w=majority`) // mongoose connection
     console.log("Database Connected");
     
